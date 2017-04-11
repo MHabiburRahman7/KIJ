f61778ed8d4e22e9bd980756311a4ec6e527f03e
MHR_7 <george_the@ymail.com>
2017-04-11 20:58:30 +0700

    tambah chat server

---
+++ Chat_server/server.py
@@ -0,0 +1,43 @@
+#python version: Python 3.5 for Windows
+import socket
+import sys
+from thread import *
+
+sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print "starting server"
+
+#menerima koneksi dari semua interface
+server_address=('localhost',10300)
+client_list=list()
+
+def send_to_all_klien(message):
+    for koneksi in client_list:
+        try:
+            koneksi.sendall(message)
+        except:
+            koneksi.close()
+            client_list.remove(koneksi)
+            
+def klien(koneksi):
+    while True:
+        try:
+            data = koneksi.recv(1024)
+            if data:
+                send_to_all_klien(data)
+            else:
+                break
+        except:
+            koneksi.close()
+            break
+            
+sock.bind(server_address)
+sock.listen(1)
+
+"starting to listen"
+while True:
+    koneksi,addr=sock.accept()
+    client_list.append(koneksi)
+    start_new_thread(klien,(koneksi,))
+
+koneksi.close()
+sock.close()
